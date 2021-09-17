760a8c332f19443a945abfb2478479ffda703bec
高橋　宗太 <souta_takahashi@012grp.co.jp>
2021-09-17 23:12:49 +0900

    Github GraphQL取得

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // env: {
+  //   NEXT_PUBLIC_GITHUB_ACCESS_TOKEN:
+  //     process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN,
+  // },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache, } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const token = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {authorization: `Bearer ${token}`},
+  cache: new InMemoryCache(),
+});
+
+
+const MyApp =({ Component, pageProps }: AppProps)=> {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 export default MyApp
