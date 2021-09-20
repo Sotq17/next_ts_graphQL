62bd8e31c205f1176a3cfe52509208749ced3a35
高橋　宗太 <souta_takahashi@012grp.co.jp>
2021-09-20 17:17:08 +0900

    lint導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,24 @@
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache, } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import type { AppProps } from 'next/app'
 
 const token = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
-  headers: {authorization: `Bearer ${token}`},
+  headers: { authorization: `Bearer ${token}` },
   cache: new InMemoryCache(),
-});
+})
 
-
-const MyApp =({ Component, pageProps }: AppProps)=> {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
-  );
+  )
 }
 export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,121 @@
+import type { NextPage } from 'next'
+import { gql, useMutation, useQuery } from '@apollo/client'
+import { css } from '@emotion/react'
+import { Query, Mutation } from 'react-apollo'
+import { useEffect, useState } from 'react'
+
+// リポジトリ取得
+const GET_REPOSITORY = gql`
+  query {
+    viewer {
+      repositories(
+        orderBy: { field: CREATED_AT, direction: DESC }
+        first: 8
+        privacy: PUBLIC
+      ) {
+        nodes {
+          id
+          name
+          url
+          description
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`
+
+const ADD_STAR_REPOSITORY = gql`
+  mutation ($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+
+const REMOVE_STAR_REPOSITORY = gql`
+  mutation ($id: ID!) {
+    removeStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+
+type Repository = {
+  id: string
+  name: string
+  url: string
+  description: string
+  stargazers: { totalCount: number }
+  viewerHasStarred: boolean
+}
+
+const Home: NextPage = () => {
+  const { loading, error, data, refetch } = useQuery(GET_REPOSITORY)
+  const [addStar] = useMutation(ADD_STAR_REPOSITORY, {
+    onCompleted() {
+      refetch()
+    },
+  })
+  const [removeStar] = useMutation(REMOVE_STAR_REPOSITORY, {
+    onCompleted() {
+      refetch()
+    },
+  })
+
+  // const [repositories, setRepositries] = useState<Repository[]>()
+  // useEffect(() => {
+  //   console.log({data})
+  //   if (!loading && data) {
+  //     setRepositries(data.viewer.repositories?.nodes)
+  //   }
+  // }, [loading, data])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {JSON.stringify(error)}</p>
+  const repositories: Repository[] = data.viewer.repositories?.nodes
+
+  return (
+    <div>
+      {repositories?.map(repo => {
+        return (
+          <div key={repo.name}>
+            <b>Repository: {repo.name}</b>
+            <p>URL: {repo.url}</p>
+            <p>Description: {repo.description || '-'}</p>
+            <p>Stars: {repo.stargazers.totalCount || '0'}</p>
+            {/* 既にstarしてあるかどうかで出し分け */}
+            {!repo.viewerHasStarred ? (
+              <button
+                onClick={() => {
+                  addStar({ variables: { id: repo.id } })
+                }}
+              >
+                star ☆
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  removeStar({ variables: { id: repo.id } })
+                }}
+              >
+                unstar ★
+              </button>
+            )}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Home
