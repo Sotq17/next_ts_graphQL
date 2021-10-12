9121b4873fc8e3e998c532492462ed7de595af9c
高橋　宗太 <souta_takahashi@012grp.co.jp>
2021-10-12 18:47:36 +0900

    store/index.tsの位置がおかしいので調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
-import store from '../store/slices'
+import store from '../store'
 import GlobalStyle from '../style/GlobalStyle'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
